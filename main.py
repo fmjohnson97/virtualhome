a1e4655a7b1530961190864fa7a91d5cd4708175
Faith Johnson <fmjohnson97@aol.com>
2021-02-24 19:57:51 -0500

    started getting GT micro actions

--- main.py
+++ main.py
@@ -10,9 +10,10 @@ from rl_utils import *
 import random
 from simulation.environment import utils as utils_environment
 from hierarchical_IL_RL.agents import Manager, Worker
+from hierarchical_IL_RL.buffers import ExpertBuffer
 
 ''' Set global script variables'''
-scene_ids=[0,1,2,3,4,5,6] #ToDo: Env 2 might be broken!!!
+scene_ids=[0,1,3,4,5,6] #ToDo: Env 2 might be broken!!!
 epoch_length=100
 total_epochs=20
 
@@ -34,8 +35,10 @@ env=UnityEnvironment(env_id=random.choice(scene_ids),
 
 #actions are ['turnleft', 'walkforward', 'turnright', 'no_action']
 manager=Manager(in_chan=3,out_actions=4)#will probably need to change out actions here
-worker=Worker(in_chan=3, out_actions=4)
+# worker=Worker(in_chan=3, out_actions=4)
 
+#make expert buffer
+exBuff=ExpertBuffer(500)
 
 ''' Go through each epoch'''
 for e in range(total_epochs):
@@ -46,11 +49,11 @@ for e in range(total_epochs):
     location, loc_id = random.choice(destinations)
     destinations.pop(destinations.index((location,loc_id)))
     for step in range(epoch_length):
-        import pdb; pdb.set_trace()
+        # import pdb; pdb.set_trace()
         obs=env.get_observations()
         #obs[1] is the input for the 1st person POV image from the following agent
-        x=manager(torch.FloatTensor(obs[1]).T.unsqueeze(0))
-        x2=worker(torch.FloatTensor(obs[1]).T.unsqueeze(0))
+        action=torch.argmax(manager(torch.FloatTensor(obs[1]).T.unsqueeze(0)),-1)
+        # x2=worker(torch.FloatTensor(obs[1]).T.unsqueeze(0),goal.detach())
 
         curr_graph = env.get_graph()
         agentids = [2]
@@ -60,6 +63,11 @@ for e in range(total_epochs):
         action_name='walktowards'
         action_str = utils_environment.can_perform_action(action_name, loc_id, 0, curr_graph, {},
                                                           teleport=False)
+        # import pdb; pdb.set_trace()
+        s, graph = env.comm.environment_graph()
+        for exp in getActionTranslation(agentids[0],loc_id,graph):
+            exBuff.add(exp)
+
         #actions need to look like
         # '<char1> [walktowards]  <milk> (258)'
         # print(action_str)
--- rl_utils.py
+++ rl_utils.py
@@ -1,7 +1,26 @@
-
+import numpy as np
 
 def get_destinations(graph_list):
     #can go to Furniture, Appliances, Rooms,
     indices=[i for i,g in enumerate(graph_list) if g['category'] in ['Rooms']]
     features=[(g['class_name'],g['id']) for i,g in enumerate(graph_list) if i in indices]
     return features
+
+def getActionTranslation(agent, destination,graph):
+    # ToDo: this function translates what "walk towards" means in terms of the human agent's motion (not the follower)
+    # gets approx diff in position between agent and destination and determines if its a walk towards, left, or right action
+    # for position: Y is upright
+
+    # import pdb; pdb.set_trace()
+    print([x for x in graph['nodes'] if x['id']==agent])
+    print([x for x in graph['nodes'] if x['id'] == destination])
+    agentPos=[x['obj_transform']['position'] for x in graph['nodes'] if x['id']==agent]
+    destPos = [x['obj_transform']['position'] for x in graph['nodes'] if x['id'] == destination]
+    # print(agentPos)
+    # print(destPos)
+    actions=[]
+    posDiff=np.array(agentPos) - np.array(destPos)
+    posDiff=posDiff[0]
+    print(np.arctan2(posDiff[-1],posDiff[0])*180/np.pi, agentPos, destPos)
+    # import pdb; pdb.set_trace()
+    return actions
